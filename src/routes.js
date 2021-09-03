ef3c8b0c92549a9fde96116458813fdd272d643f
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-09-03 15:29:37 -0300

    Remove Test component and its route

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
-import Test from './pages/Test';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Main} />
-        <Route exact path="/test" component={Test} />
       </Switch>
     </BrowserRouter>
   )
