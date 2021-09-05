72f3de6a4d1565ec759ff47591a6a691ef3bca37
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-09-05 13:16:07 -0300

    feat: create pagination

--- src/components/CatList/index.js
+++ src/components/CatList/index.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { CatCardStyled } from "./styles";
 
-export default function CatList({ cats }) {
+export default function CatList({ cats, page, count }) {
     const [expandedIndex, setExpandedIndex] = useState(0);
     const [willCloseCard, setWillCloseCard] = useState(false);
+    const [paginatedCats, setPaginatedCats] = useState([]);
+
+    useEffect(() => {
+        let perPage = 5;
+        let pageIndexed = page - 1;
+
+        let start = pageIndexed * perPage;
+        let end = start + perPage;
+
+        setPaginatedCats(cats.slice(start, end));
+    }, [page, cats]);
 
     const handleClick = (clickedIndex) => {
 
@@ -21,7 +32,7 @@ export default function CatList({ cats }) {
     return (
         <>
             {
-                cats.map((cat, index) => (
+                paginatedCats.map((cat, index) => (
                     <CatCardStyled
                         onClick={() => handleClick(index)}
                         expanded={!willCloseCard ? index === expandedIndex : false}
