94535f4aa32b7ab892ced22ded6abbf60adc15b9
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-09-06 11:29:23 -0300

    feat: implement loading

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Header, MainPage, Paginate } from './styles';
 import CatList from '../../components/CatList';
+import Loading from '../../components/Loading';
 
 import CatRepository from "../../repositories/CatRepository";
 import { usePagination } from '../../contexts/PaginationContext';
@@ -8,13 +9,18 @@ import { usePagination } from '../../contexts/PaginationContext';
 export default function Main() {
     const [results, setResults] = useState([]);
     const [search, setSearch] = useState('');
+    const [loading, setLoading] = useState(false);
     const { page, numberOfPages, setPage, setItems } = usePagination();
 
     useEffect(() => {
         async function load() {
             try {
+                setLoading(true);
+
                 const { data } = await CatRepository.get();
                 setResults(data);
+
+                setLoading(false);
             } catch (error) {
                 console.log(error);
             }
@@ -24,9 +30,12 @@ export default function Main() {
     }, []);
 
     useEffect(() => {
+
         const filtered = !!search ? results.filter(cat => cat.name.toLowerCase().includes(search.toLowerCase())) : results;
         setItems(filtered);
+
         setPage(1);
+
     }, [results, search, setPage, setItems]);
 
     const handleChange = (event, value) => {
@@ -42,10 +51,12 @@ export default function Main() {
                     <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
                 </form>
             </Header>
-            <Paginate count={numberOfPages} page={page} onChange={handleChange} />
-            <MainPage>
-                <CatList />
-            </MainPage>
+            <Loading loading={loading} />
+            {!loading && <Paginate count={numberOfPages} page={page} onChange={handleChange} />}
+            {!loading &&
+                <MainPage>
+                    <CatList />
+                </MainPage>}
         </Container>
     )
 }
